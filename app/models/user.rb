4c62f31645bc358177f4fe3e0e4651a8529f834f
PTT1230 <yujinyoutai@gmail.com>
2022-07-28 13:33:52 +0900

    テーブル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+
+  validates :nickname,         presence: true
+  validates :birthday,         presence: true
+  validates :last_name_kana,   presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_name_kanji,  presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name_kanji, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
 end
