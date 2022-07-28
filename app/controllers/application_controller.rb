94ff92c91354e9429cb40cddd7dee08f95ea07d2
PTT1230 <yujinyoutai@gmail.com>
2022-07-28 20:32:54 +0900

    新規登録・ログイン・ログアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  
 
 
 
@@ -13,6 +13,7 @@ class ApplicationController < ActionController::Base
     end
   end
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name_kana, :first_name_kana, :last_name_kanji, :first_name_kanji, :birthday,])
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [:nickname, :last_name_kana, :first_name_kana, :last_name_kanji, :first_name_kanji, :birthday])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,11 @@ class User < ApplicationRecord
 
   has_many :items
 
-  validates :nickname,         presence: true
-  validates :birthday,         presence: true
-  validates :last_name_kana,   presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :last_name_kanji,  presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name_kanji, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :nickname,          presence: true
+  validates :birthday,          presence: true
+  validates :last_name_kana,    presence: true#, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana,   presence: true#, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_name_kanji,   presence: true#, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name_kanji,  presence: true#, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  #validates :encrypted_password,presence: true
 end
