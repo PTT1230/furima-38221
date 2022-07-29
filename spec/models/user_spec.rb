c5385725197c5aa59c320671c197dddfa197b2fc
PTT1230 <yujinyoutai@gmail.com>
2022-07-29 10:55:20 +0900

    ダブり修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,12 +33,6 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
-      it 'passwordとpassword_confirmationが不一致では登録できない' do
-        @user.password = '123456'
-        @user.password_confirmation = '1234567'
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
-      end
       it 'Birthdayが空だと登録できない' do
         @user.birthday = ''
         @user.valid?
