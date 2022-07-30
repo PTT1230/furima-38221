9f885cf34ef39b2f04712503242e6880a25f3c11
PTT1230 <yujinyoutai@gmail.com>
2022-07-30 19:07:01 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,3 +5,10 @@ class ItemsController < ApplicationController
   def new
     #@item = Item.new
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  end
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
   #has_one :shopping
   belongs_to :user
+  has_one_attached :image
 end
