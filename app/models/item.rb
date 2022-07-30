20d445d93c2ef5b75fb66207d1ca63141ed0e5ac
PTT1230 <yujinyoutai@gmail.com>
2022-07-31 08:15:04 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,26 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shopping_fee
+  belongs_to :area
+  belongs_to :delivery_date
   #has_one :shopping
   belongs_to :user
   has_one_attached :image
+
+  validates :category_id,      numericality: { other_than: 1, message: "can't be blank" }
+  validates :category_id,      numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id,     numericality: { other_than: 1, message: "can't be blank" }
+  validates :shopping_fee_id,  numericality: { other_than: 1, message: "can't be blank" }
+  validates :area_id,          numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_date_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :item_name,        presence: true
+  validates :price,            numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                               format: { with: /\A[0-9]+\z/ }
+  validates :explanation,      presence: true
+  validates :user,             presence: true
 end
+
+#presence: true, format: { with: /\A[0-9]+\z/ , /\A[+-]?\d+\z/ }, length: { minimum: 300, maximum: 9999999 }, numericality: {only_integer: true }
+#数値を通貨のフォーマットに変換少数以下は無視number_to_currency :strip_insignificant_zeros	
--- db/migrate/20220730063949_create_items.rb
+++ db/migrate/20220730063949_create_items.rb
@@ -2,14 +2,14 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
 
-      t.integer    :category_id,   null: false
+      t.integer :category_id,      null: false
       t.integer :condition_id,     null: false
       t.integer :shopping_fee_id,  null: false 
       t.integer :area_id,          null: false
       t.integer :delivery_date_id, null: false
-      t.string :item_name,         null: false
+      t.string  :item_name,        null: false
       t.integer :price,            null: false
-      t.text :explanation,         null: false
+      t.text    :explanation,      null: false
       t.references :user,          null: false, foreign_key: true
       t.timestamps
     end
