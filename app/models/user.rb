01bff3072e1ce8c67ce256e5d96424b8f1e0c470
PTT1230 <yujinyoutai@gmail.com>
2022-07-30 14:30:48 +0900

    email一意性削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,6 @@ class User < ApplicationRecord
   validates :first_name_kana,   presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :last_name_kanji,   presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :first_name_kanji,  presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-  validates :email,             uniqueness: true
   validates :password,          format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
 
   #validates :password, format: { with: /\A[a-zA-Z]+\z/ }
