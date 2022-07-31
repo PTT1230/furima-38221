df36e97eb2be4509b371abcb9caf10be6ab6e1ec
PTT1230 <yujinyoutai@gmail.com>
2022-07-31 14:57:39 +0900

    金額入力時の計算

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,21 @@
-window.addEventListener('load', () => {
-  console.log("OK");
-});
+//const priceInput = document.getElementById("item-price");
+//priceInput.addEventListener("input", () => {
+   //console.log("イベント発火");
+//})
+
+//const priceInput = document.getElementById("item-price");
+//console.log(priceInput);
+
+//window.addEventListener('load', () => {
+  //console.log("OK");
+//});
+window.addEventListener('load', function(){
+const priceInput = document.getElementById("item-price");
+priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor( parseInt(inputValue) * 0.1 );
+    const profitDom = document.getElementById('profit');
+    profitDom.innerHTML = Math.floor( parseInt(inputValue) - parseInt(addTaxDom.innerHTML) );
+  })
+})
