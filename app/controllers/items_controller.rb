e1034b137f69513cca1e90c54c223c20afb505a4
PTT1230 <yujinyoutai@gmail.com>
2022-08-03 07:35:00 +0900

    再修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,10 +36,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.user_id != current_user.id
+    if current_user.id == @item.user_id
+      @item.destroy
       redirect_to root_path
     else
-      @item.destroy
+      render :index 
     end
   end
 
